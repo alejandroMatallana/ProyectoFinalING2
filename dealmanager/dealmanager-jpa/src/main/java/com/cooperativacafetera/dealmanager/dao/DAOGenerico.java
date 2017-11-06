a4721f88aeb25790f5cc6a5a4ef732361d470633
Alejandro <alejito.matallana@gmail.com>
2017-11-06 15:20:49 -0500

    Cambios en el DAO GENERICO

--- dealmanager/dealmanager-jpa/src/main/java/com/cooperativacafetera/dealmanager/dao/DAOGenerico.java
+++ dealmanager/dealmanager-jpa/src/main/java/com/cooperativacafetera/dealmanager/dao/DAOGenerico.java
@@ -2,6 +2,8 @@ package com.cooperativacafetera.dealmanager.dao;
 
 import java.util.List;
 
+
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -16,23 +18,43 @@ public class DAOGenerico {
 		this.clase = clase;
 	}
 
+	//Metodo generico para crear un objeto de cualquier tipo
 	public void crear(Object obj) {
 		em.persist(obj);
 	}
 
+	/**
+	 * Metodo generico para editar un objeto de cualquier tipo
+	 * @param obj
+	 */
 	public void editar(Object obj) {
 		em.merge(obj);
 	}
 
+	/**
+	 * Metodo generico que permite eliminar un objeto
+	 * @param obj
+	 */
 	public void eliminar(Object obj) {
 		em.remove(em.contains(obj) ? obj : em.merge(obj));
 	}
 
+	/**
+	 * Metodo para buscar un objeto por su llave primaria
+	 * @param pk
+	 * @return, el objeto si se encuentra
+	 */
 	public <T> T buscar(Object pk) {
 
 		return (T) em.find(clase, pk);
 	}
 
+	/**
+	 * Metodo para listar los objetos guardados en la base de datos
+	 * @param nombre
+	 * @param parametros
+	 * @return, la lista con lo que este en la BD
+	 */
 	public <T> List<T> ejecutarNamedQuery(String nombre, Object... parametros) {
 		Query q = em.createNamedQuery(nombre);
 		for (int i = 0; i < parametros.length; i++) {
